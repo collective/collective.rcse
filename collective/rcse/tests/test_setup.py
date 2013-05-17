40b43e36a17702a6f73a9a8dd0b5f484ac23d537
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-05-17 13:28:30 +0200

    test: add test_setup of dependencies
    Now we check if all dependencies are installed by the quickinstaller

--- collective/rcse/tests/test_setup.py
+++ collective/rcse/tests/test_setup.py
@@ -6,9 +6,22 @@ class TestSetup(base.IntegrationTestCase):
     """We tests the setup (install) of the addons. You should check all
     stuff in profile are well activated (browserlayer, js, content types, ...)
     """
-
-    def test_xxx(self):
-        self.assertTrue(False)
+    def test_dependencies_installed(self):
+        qi = self.portal.portal_quickinstaller
+        dependencies = [
+            "cioppino.twothumbs",
+            "collective.etherpad",
+            "collective.history",
+            "collective.js.ckeditor",
+            "collective.mediaelementjs",
+            "collective.picturefill",
+            "collective.themeswitcher",
+            "plone.app.dexterity",
+            "plonetheme.foundation",
+            "plonetheme.jquerymobile",
+        ]
+        for name in dependencies:
+            self.assertTrue(qi.isProductInstalled(name))
 
 
 def test_suite():
