8bfecc864ed98221466c904f4b8a497da123b868
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-05-21 17:48:08 +0200

    display(group tile): add effective date
    The effective date is extracted in order of found date:
    * effective_date
    * modification_date
    * creation_date

--- collective/rcse/content/group.py
+++ collective/rcse/content/group.py
@@ -64,6 +64,20 @@ class GroupTileView(BrowserView):
         self.group = self.context.aq_inner.aq_parent
         self.group_url = self.group.absolute_url()
         self.group_title = self.group.Title()
+        self.effective_date = self.get_effective_date()
 
     def get_content(self):
         return self.context.restrictedTraverse('tile_view')()
+
+    def get_effective_date(self):
+        effective = None
+        if self.context.effective_date is not None:
+            effective = self.context.effective_date
+        elif self.context.modification_date is not None:
+            effective = self.context.modification_date
+        elif self.context.creation_date is not None:
+            effective = self.context.creation_date
+
+        if effective is not None:
+            return effective.strftime("%d-%m-%Y")
+        return ""
