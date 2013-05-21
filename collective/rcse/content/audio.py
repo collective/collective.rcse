fe6c413110e41f159d4ef9d9c23dbe11da36aa98
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-05-21 09:10:28 +0200

    Fix (imports): update zcml import according to ptheme.jqm

--- collective/rcse/content/audio.py
+++ collective/rcse/content/audio.py
@@ -23,14 +23,24 @@ class AudioView(MediaElementJS):
         return self.index()
 
     def update(self):
-        self.sources = []
+        self._audio = {}
         context_url = self.context.absolute_url()
         filename = self.context.file.filename.encode('utf-8')
         mimetype = self.context.file.contentType
         source = {}
         source["src"] = context_url + '/@@download/file/' + filename
         source["mimetype"] = mimetype
-        self.sources.append(source)
+        source["duration"] = ""
+        self.source = source
+
+    def href(self):
+        return self.source["src"]
+
+    def audio(self):
+        return self.source
 
     def getFilename(self):
         return self.context.file.filename.encode('utf-8')
+
+    def getContentType(self):
+        return self.context.file.contentType
