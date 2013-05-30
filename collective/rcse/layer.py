82a21ae57c998a2cec2e6a14eb01de8ba935f37e
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-05-30 17:58:46 +0200

    style: override template of discussion item

--- collective/rcse/layer.py
+++ collective/rcse/layer.py
@@ -1,10 +1,11 @@
 from plonetheme.foundation.browser.layer import Layer as FoundationLayer
 from plonetheme.jquerymobile.layer import Layer as JQMobileLayer
+from plone.app.discussion.interfaces import IDiscussionLayer
 
 
-class MobileLayer(JQMobileLayer):
+class MobileLayer(JQMobileLayer, IDiscussionLayer):
     """Marker interface which is used in the mobile theme."""
 
 
-class DesktopLayer(FoundationLayer):
+class DesktopLayer(FoundationLayer, IDiscussionLayer):
     """Marker interface which is used in the desktop theme"""
