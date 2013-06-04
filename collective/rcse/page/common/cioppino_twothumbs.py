55094b71c6d4e483e3475437aed37f103651f4ef
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-06-04 18:18:56 +0200

    refactor: like/dislike feature are now document_actions

---
+++ collective/rcse/page/common/cioppino_twothumbs.py
@@ -0,0 +1,32 @@
+from cioppino.twothumbs import rate
+from Products.Five.browser import BrowserView
+from Products.statusmessages.interfaces import IStatusMessage
+from cioppino.twothumbs import _
+
+
+class Like(BrowserView):
+    """Like the context"""
+    def __call__(self):
+        action = rate.loveIt(self.context)
+        status = IStatusMessage(self.request)
+        if action == "like":
+            msg = _(u"You liked this. Thanks for the feedback!")
+        else:
+            msg = _(u"Your vote has been removed.")
+        status.add(msg)
+        object_url = self.context.absolute_url()
+        self.request.response.redirect(object_url)
+
+
+class DisLike(BrowserView):
+    """DisLike the context"""
+    def __call__(self):
+        action = rate.hateIt(self.context)
+        status = IStatusMessage(self.request)
+        if action == "dislike":
+            msg = _(u"You dislike this. Thanks for the feedback!")
+        else:
+            msg = _(u"Your vote has been removed.")
+        status.add(msg)
+        object_url = self.context.absolute_url()
+        self.request.response.redirect(object_url)
