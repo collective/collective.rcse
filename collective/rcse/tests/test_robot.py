3879557d483e67916cfb41825ac6b2cbfc4acf33
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-06-06 22:40:48 +0200

    refactor(test): rename robot tests

--- collective/rcse/tests/test_robot.py
+++ collective/rcse/tests/test_robot.py
@@ -8,7 +8,7 @@ from plone.testing import layered
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
-        layered(robotsuite.RobotTestSuite('test_functional.robot'),
+        layered(robotsuite.RobotTestSuite('test_robot.robot'),
                 layer=ROBOT),
     ])
     return suite
