f06de91e6861ed68736fff58c01116fe6db058dd
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-06-12 15:07:56 +0200

    feature: add security configuration in registry
    Let the administrator choose which role can add groups

---
+++ collective/rcse/security.py
@@ -0,0 +1,32 @@
+from zope import component
+from zope import interface
+from zope import schema
+from plone.registry.interfaces import IRecordModifiedEvent
+from collective.rcse import i18n
+
+_ = i18n._
+
+
+class ISecuritySettings(interface.Interface):
+    """We handle permissions settings throw a control-panel for an high level
+    overview of what can be done by who"""
+
+    addGroupPermission = schema.Choice(
+        title=_(u"Add group"),
+        vocabulary="plone.app.vocabularies.Roles",
+        default=False,
+    )
+
+
+@component.adapter(ISecuritySettings, IRecordModifiedEvent)
+def handle_security_update(records, event):
+    record = event.record
+    site = component.hooks.getSite()
+    if record.fieldName == 'addGroupPermission':
+        permission = "Add portal content"
+        permission_settings = site.permission_settings(permission=permission)
+        p_acquire = permission_settings[0]["acquire"]
+        roles = ["Manager"]
+        if event.newValue != "Manager":
+            roles.append(event.newValue)
+        site.manage_permission(permission, roles=roles, acquire=p_acquire)
