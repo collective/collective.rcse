87953f52335bd7f1e5b9ab4fd6919eb5cf9a52b1
Yann Fouillat <yann.fouillat@gmail.com>
2013-06-21 16:17:03 +0200

    feat: add collective.whathappened

---
+++ collective/rcse/viewlet/mobile/viewlets.py
@@ -0,0 +1,6 @@
+from plonetheme.jquerymobile.browser.viewlets import personal_bar
+from collective.whathappened.browser.notifications import HotViewlet
+
+
+class RcsePersonalBarViewlet(personal_bar.PersonalBarViewlet, HotViewlet):
+    pass
