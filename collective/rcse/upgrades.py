05b438bfe948bb0a51d54edc37941fc2c7ee23d0
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-07-05 11:11:18 +0200

    fix: add upgrade step for group owner that must be site admin

--- collective/rcse/upgrades.py
+++ collective/rcse/upgrades.py
@@ -5,3 +5,16 @@ PROFILE = 'profile-collective.rcse:default'
 def common(context):
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile(PROFILE)
+
+
+def make_group_owner_siteadmin(context):
+    import pdb;pdb.set_trace()
+    catalog = getToolByName(context, 'portal_catalog')
+    brains = catalog(portal_type="collective.rcse.group")
+    roles = ["Owner", "Site Administrator"]
+
+    for brain in brains:
+        group = brain.getObject()
+        creators = group.creators
+        for creator in creators:
+            group.manage_setLocalRoles(creator, roles)
