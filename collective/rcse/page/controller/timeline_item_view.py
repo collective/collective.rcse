a56f11ae51f9a3dd418a9e4b47ec912822719e9d
Yann Fouillat <yann.fouillat@gmail.com>
2013-07-15 11:36:29 +0200

    feat: add actions to tiles

--- collective/rcse/page/controller/timeline_item_view.py
+++ collective/rcse/page/controller/timeline_item_view.py
@@ -2,6 +2,11 @@ from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 from plone.uuid.interfaces import IUUID
 from Products.ZCatalog.interfaces import ICatalogBrain
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+
+from collective.rcse.settings import IDocumentActionsIcons
 
 
 class TimelineItemView(BrowserView):
@@ -25,6 +30,8 @@ class TimelineItemView(BrowserView):
         self.group_title = None
         self.effective_date = None
         self.creator_info = None
+        self.actions = None
+        self.actions_icon = None
 
     def update(self):
         if self.membership is None:
@@ -44,9 +51,30 @@ class TimelineItemView(BrowserView):
         if self.creator_info is None:
             name = "@@creator_memberinfo"
             self.creator_info = self.context.restrictedTraverse(name)
+        if self.actions is None:
+            self.context_state = getMultiAdapter((self.context, self.request),
+                                                 name=u'plone_context_state')
+            self.actions = self.context_state.actions('document_actions')
+        if self.actions_icon is None:
+            self._getActionsIcons()
 
     def get_content(self):
         return self.context.restrictedTraverse('tile_view')()
+
+    def _getActionsIcons(self):
+        reg = getUtility(IRegistry)
+        config = reg.forInterface(IDocumentActionsIcons, False)
+        if not config or not hasattr(config, 'mapping'):
+            return
+        self.mapping = config.mapping
+        self.actions_icon = []
+        for action in self.actions:
+            if action['id'] in self.mapping.keys():
+                action = action
+                action['icon'] = self.mapping[action['id']]
+                self.actions_icon.append(action)
+        for action in self.actions_icon:
+            self.actions.remove(action)
 
     def get_effective_date(self):
         effective = None
