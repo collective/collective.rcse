5aaae68c53cbc3bc2d5154558e2b4b3d82b74bbd
Gagaro <gagaro42@gmail.com>
2013-07-30 15:15:05 +0200

    feat: add group audio and video views

--- collective/rcse/content/audio.py
+++ collective/rcse/content/audio.py
@@ -1,5 +1,12 @@
 from plone.directives import form
+from plone.namedfile.field import NamedBlobFile
+from collective.rcse.i18n import RCSEMessageFactory
+
+_ = RCSEMessageFactory
 
 
 class AudioSchema(form.Schema):
-    """An audio file"""
+    """An audio file.
+    """
+
+    file = NamedBlobFile(title=_(u"Audio file"))
--- collective/rcse/content/video.py
+++ collective/rcse/content/video.py
@@ -1,5 +1,11 @@
 from plone.directives import form
+from plone.namedfile.field import NamedBlobFile
+from collective.rcse.i18n import RCSEMessageFactory
+
+_ = RCSEMessageFactory
 
 
 class VideoSchema(form.Schema):
     """A video file"""
+
+    file = NamedBlobFile(title=_(u"Video file"))
