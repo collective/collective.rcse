db1dacd3660ccf356c250438ae5607f9570421c4
Gagaro <gagaro42@gmail.com>
2013-07-30 18:02:00 +0200

    feat: add blog view and image view

--- collective/rcse/page/controller/group_blog_view.py
+++ collective/rcse/page/controller/group_blog_view.py
@@ -3,4 +3,4 @@ from collective.rcse.page.controller.group_base import BaseView
 
 class BlogView(BaseView):
     """A filterable timeline"""
-    filter_type = ["Document"]
+    filter_type = ["News Item"]
