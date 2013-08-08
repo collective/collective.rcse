9926ada6b2bfcc0c704185926cdea54008396a59
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-08-08 14:36:07 +0200

    feature(member): check the validity of a member.
    if a member is not valid, it will be redirected somewhere.

--- collective/rcse/page/controller/personal-information.py
+++ collective/rcse/page/controller/personal-information.py
@@ -10,22 +10,24 @@ class PersonalInformation(BrowserView):
         portal_url = getToolByName(self.context, 'portal_url')
         mtool = getToolByName(self.context, 'portal_membership')
         member = mtool.getAuthenticatedMember()
-        if member.getId() == 'admin':
+        #fix bugs
+        username = member.getProperty('username')
+        if username == 'admin':
             self.request.response.redirect(
                 '%s/users_directory' % portal_url()
                 )
-        elif type(member.getProperty('username')) == object:
+        elif type(username) == object:
             self.request.response.redirect(
                 '%s/@@register_information' % portal_url()
                 )
         else:
             catalog = getToolByName(self.context, 'membrane_tool')
-            results = catalog(getUserName=member.getProperty('username'))
+            results = catalog(getUserName=username)
             if len(results) == 0:
                 self.request.response.redirect(
                     '%s/users_directory' % portal_url()
                     )
             else:
                 self.request.response.redirect(
-                    results[0].getURL()
+                    '%s/edit' % results[0].getURL()
                     )
---
+++ collective/rcse/viewlet/controller/validate_authenticated_member.py
@@ -0,0 +1,108 @@
+import logging
+from Acquisition import aq_inner
+from zope import component
+from zope import schema
+from plone.app.layout.viewlets.common import ViewletBase
+from collective.rcse.content.member import IMember
+from dexterity.membrane.behavior.membraneuser import IMembraneUser
+from Products.CMFCore.utils import getToolByName
+from Products.statusmessages.interfaces import IStatusMessage
+from collective.rcse.i18n import _
+logger = logging.getLogger("collective.rcse")
+
+
+class ValidateAuthenticatedMember(ViewletBase):
+    """This viewlet will redirect to @@personal_information if the user
+    miss some information
+    """
+    member_schema = IMember
+    viewname = "@@personal-information"
+    blacklist_views = []
+
+    def update(self):
+        super(ValidateAuthenticatedMember, self).update()
+        self.context_state =  component.getMultiAdapter(
+            (self.context, self.request), name=u'plone_context_state'
+        )
+        self.member = self.portal_state.member()
+        #get member data
+        catalog = getToolByName(self.context, 'membrane_tool')
+        self.username = self.member.getUserName()
+        self.member_data = None
+        if self.username:
+            results = catalog(getUserName=self.username)
+            if results:
+                self.member_data = results[0].getObject()
+        self.status = IStatusMessage(self.request)
+
+    def index(self):
+        if self.portal_state.anonymous():
+            return ''
+        elif self.username == 'admin':
+            return ''
+        elif self.view_in_blacklist():
+            return ''
+        elif not self.is_registred():
+            msg = _(u"You are not registred, please proceed")
+            self.status.add(msg)
+            url =  '%s/@@register' % (self.portal_state.portal_url())
+            self.lock_rendering_and_redirect(url=url)
+            return ''
+        elif IMembraneUser.providedBy(self.context) and self.context.username == self.member.getUserName():
+                return ''
+        elif not self.has_required_info():
+            msg = _(u"Your profile is missing some required information")
+            self.status.add(msg)
+            url =  '%s/edit' % (self.member_data.absolute_url())
+            self.lock_rendering_and_redirect(url=url)
+            return ''
+        elif not self.is_validated_user():
+            msg = _(u"Your profile is waiting for approval")
+            self.status.add(msg)
+            url =  self.member_data.absolute_url()
+            self.lock_rendering_and_redirect(url=url)
+            return ''
+
+        return ''
+
+    def has_required_info(self):
+        has_required_info = True
+        #we will check all required field against the schema
+        required_fields = []
+        fields = schema.getFields(self.member_schema)
+        for field_name, field in fields.iteritems():
+            if field.required:
+                required_fields.append(field_name)
+
+        for field_name in required_fields:
+            if getattr(self.member_data, field_name, None) is None:
+                has_required_info = False
+                break
+
+        return has_required_info
+
+    def is_registred(self):
+        return self.member_data is not None
+
+    def is_validated_user(self):
+        tool = getToolByName(self.context, "portal_workflow")
+        data = aq_inner(self.member_data)
+        return tool.getInfoFor(data, 'review_state', None) == "enabled"
+
+    def lock_rendering_and_redirect(self, url):
+        self.request.response.redirect(url, lock=True)
+        self.request.response.setBody('', lock=True)
+
+    def view_in_blacklist(self):
+        viewname = self.request.get('URL').split('/')[-1]
+        #login / logout
+        if viewname in ('login', 'login_form', 'logout', 'logged_out'):
+            return True
+        #registration
+        if viewname in ('@@register', '@@personal-information', '@@register_information'):
+            return True
+        if viewname in self.blacklist_views:
+            return True
+        if viewname == self.viewname:
+            return True
+        return False
