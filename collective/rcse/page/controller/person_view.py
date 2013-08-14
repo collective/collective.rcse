bf8f80d937d2aba9f65d9ef1fa80e443dfef2fbe
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-08-14 18:50:40 +0200

    refactor(person_view): use dynamic getattributes + add privacy

--- collective/rcse/page/controller/person_view.py
+++ collective/rcse/page/controller/person_view.py
@@ -1,18 +1,22 @@
 from AccessControl import getSecurityManager
 from zope import component
 from zope import interface
+from zope import schema
 from Products.Five.browser import BrowserView
 from Products.CMFCore.permissions import ReviewPortalContent
 from Products.CMFCore.utils import getToolByName
+from plone.memoize.instance import memoize
 
 from collective.rcse.content import member
 from collective.rcse.i18n import _
+from collective.rcse.content.visibility import NAMESPACE
 
 
 class AuthenticatedMemberInfoView(BrowserView):
     """Person view helper to display member info"""
     interface.implements(member.IMember)
     member_schema = member.IMember
+    member_fields = schema.getFieldNames(member.IMember)
 
     def __call__(self):
         self.update()
@@ -24,19 +28,25 @@ class AuthenticatedMemberInfoView(BrowserView):
         BrowserView.__init__(self, context, request)
         #dependencies
         self.portal_url = None
+        self.membrane_tool = None
         self.membership = None
         self.memberid = None
         self.member = None
-
-        #info
-        self.fullname = None
         self.url = None
-        self.dialog_url = None
-        self.photo = None
-        self.company = None
-        self.function = None
-        self.professional_email = None
-        self.professional_mobile_phone = None
+        self.fullname = None
+
+    @memoize
+    def get_membrain(self):
+        """return the member as a brain (Products.Membrane)"""
+        brains = self.membrane_tool(exact_getUserId=self.memberid)
+        if len(brains) == 1:
+            return brains[0]
+
+    @memoize
+    def get_membrane(self):
+        brain = self.get_membrain()
+        if brain:
+            return brain.getObject()
 
     def update(self):
         self.update_dependencies()
@@ -48,6 +58,8 @@ class AuthenticatedMemberInfoView(BrowserView):
             self.portal_url = getToolByName(self.context, "portal_url")
         if self.membership is None:
             self.membership = getToolByName(self.context, "portal_membership")
+        if self.membrane_tool is None:
+            self.membrane_tool = getToolByName(self.context, 'membrane_tool')
 
     def update_member(self):
         if self.member is None:
@@ -60,24 +72,30 @@ class AuthenticatedMemberInfoView(BrowserView):
             raise ValueError("member can t be none")
         if self.url is None:
             self.url = self.portal_url() + '/author/' + self.memberid
-        if self.photo is None:
-            photo = self.membership.getPersonalPortrait(self.memberid)
-            if photo:
-                self.photo = photo.absolute_url()
+        if self.fullname is None:
+            self.fullname = self.member.getProperty('fullname')
+
+    def __getattribute__(self, name):
+        if name in AuthenticatedMemberInfoView.member_fields:
+            membrane = self.get_membrane()
+            pref = '%s%s' % (NAMESPACE, name)
+            if getattr(membrane, pref, True):
+                return getattr(membrane, name)
             else:
-                #TODO: replace by gravatar
-                path = '/++resource++collective.rcse/defaultUser.png'
-                self.photo = self.portal_url() + path
+                return ""
+        else:
+            return BrowserView.__getattribute__(self, name)
 
-        attributes = list(self.member_schema.names())
-        attributes += ['fullname']
+    def photo(self):
+        membrane = self.get_membrane()
+        if membrane:
+            avatar = membrane.avatar
+            if avatar:
+                return avatar.absolute_url()
 
-        for attr in attributes:
-            if getattr(self, attr, None) is None:
-                value = self.member.getProperty(attr)
-                if type(value) == object:
-                    value = None
-                setattr(self, attr, value)
+        #TODO: replace by gravatar
+        path = '/++resource++collective.rcse/defaultUser.png'
+        return self.portal_url() + path
 
 
 class CreatorMemberInfoView(AuthenticatedMemberInfoView):
@@ -88,6 +106,15 @@ class CreatorMemberInfoView(AuthenticatedMemberInfoView):
         if self.member is None and self.memberid is not None:
             self.member = self.membership.getMemberById(self.memberid)
 
+
+class RequestMemberInfoView(AuthenticatedMemberInfoView):
+    """member info throw request param
+    """
+    def update_member(self):
+        if self.memberid is None:
+            self.memberid = self.request.get("memberid", None)
+        if self.member is None and self.memberid is not None:
+            self.member = self.membership.getMemberById(self.memberid)
 
 class MemberInfoView(BrowserView):
     def __init__(self, context, request):
