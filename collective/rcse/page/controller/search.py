1f2b8cbc811aabb91ac311eb1f68fcc70ee101a5
Gagaro <gagaro42@gmail.com>
2013-09-20 16:23:19 +0200

    fix: #10065 sort_order in search

--- collective/rcse/page/controller/search.py
+++ collective/rcse/page/controller/search.py
@@ -1,10 +1,13 @@
 from plone.app.search import browser
 
-#THIS IS A PATCH BECAUSE IT JUST DOESN T WORK ...
+#THIS IS A PATCH BECAUSE IT JUST DOESN'T WORK ...
+# => I AGREE
 class Search(browser.Search):
     def filter_query(self, query):
         text = self.request.form.get('SearchableText', None)
         if text and type(text) is unicode:
             self.request.form['SearchableText'] = text.encode('utf-8')
+        sort_order = self.request.form.get('sort_order', None)
+        if sort_order and type(sort_order) is unicode:
+            self.request.form['sort_order'] = sort_order.encode('utf-8')
         return super(Search, self).filter_query(query)
-
