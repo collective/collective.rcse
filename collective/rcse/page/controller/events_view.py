30661ccca9d1606463805da1e633a5b0db288dcb
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-09-26 17:50:02 +0200

    feature(filters) use defintion list

--- collective/rcse/page/controller/events_view.py
+++ collective/rcse/page/controller/events_view.py
@@ -1,19 +1,57 @@
-import calendar
+from zope import interface
+from zope import schema
+from zope import component
+from z3c.form import button
 
-from plone.app.event.base import localized_today, construct_calendar
-from Products.CMFPlone.utils import getToolByName
-from zope.i18nmessageid import MessageFactory
+from plone.namedfile.field import NamedBlobImage
+from plone.supermodel import model
+from plone.app.event.dx.behaviors import IEventBasic
 
-from collective.rcse.page.controller.group_base import BaseView
+from collective.rcse.i18n import _
+from collective.rcse.page.controller import group_base
 from collective.rcse.page.controller.navigationroot import NavigationRootBaseView
 
+CONTENT_TYPE = "collective.rcse.event"
 
-PLMF = MessageFactory('plonelocales')
+
+class AddFormSchema(IEventBasic):
+    """Add form"""
+    title = schema.TextLine(title=_(u"Title"))
+    description = schema.Text(
+        title=_(u"Description"),
+    )
 
 
-class EventsView(BaseView):
-    """A view of events in a calendar"""
-    filter_type = ["collective.rcse.event"]
+class AddFormAdapter(object):
+    interface.implements(AddFormSchema)
+    component.adapts(interface.Interface)
+
+    def __init__(self, context):
+        self.context = context
+        self.title = None
+        self.description = None
+        self.start = None
+        self.end = None
+        self.whole_day = None
+        self.open_end = None
+        self.timezone = None
+
+
+class AddForm(group_base.BaseAddForm):
+    schema = AddFormSchema
+    CONTENT_TYPE = CONTENT_TYPE
+    msg_added = _(u"Event added")
+    label = _(u"Add event")
+
+    @button.buttonAndHandler(_(u"Add event"))
+    def handleAdd(self, action):
+        group_base.BaseAddForm.handleAdd(self, action)
+
+
+class EventsView(group_base.BaseAddFormView):
+    """A filterable blog view"""
+    filter_type = [CONTENT_TYPE]
+    form = AddForm
 
 
 class NavigationRootEventsView(EventsView, NavigationRootBaseView):
--- collective/rcse/page/controller/videos_view.py
+++ collective/rcse/page/controller/videos_view.py
@@ -1,24 +1,58 @@
-from collective.rcse.page.controller.group_base import BaseView
+from zope import interface
+from zope import schema
+from zope import component
+from z3c.form import button
+
+from plone.autoform import directives as form
+from plone.namedfile.field import NamedBlobImage
+from plone.supermodel import model
+
+from collective.rcse.i18n import _
+from collective.rcse.page.controller import group_base
 from collective.rcse.page.controller.navigationroot import NavigationRootBaseView
+from collective.rcse.content.video import VideoSchema
+
+CONTENT_TYPE = "collective.rcse.video"
 
 
-class VideosView(BaseView):
-    """A filterable timeline"""
-    filter_type = ["collective.rcse.video"]
+class AddFormSchema(VideoSchema):
+    """Add form"""
 
-    def getVideos(self):
-        results = self.get_content(batch=False)
-        videos = []
-        for result in results:
-            obj = result.getObject()
-            video = {}
-            video["src"] = obj.absolute_url() + '/@@download/file/'
-            video["src"] += obj.file.filename.encode('utf-8')
-            video["title"] = obj.title
-            video["description"] = obj.description
-            video["mimetype"] = obj.file.contentType
-            videos.append(video)
-        return videos
+    title = schema.TextLine(title=_(u"Title"))
+
+    description = schema.Text(
+        title=_(u"Description"),
+    )
+    form.order_before(title='*')
+    form.order_before(description='file')
+
+
+class AddFormAdapter(object):
+    interface.implements(AddFormSchema)
+    component.adapts(interface.Interface)
+
+    def __init__(self, context):
+        self.context = context
+        self.title = None
+        self.file = None
+        self.remoteUrl = None
+
+
+class AddForm(group_base.BaseAddForm):
+    schema = AddFormSchema
+    CONTENT_TYPE = CONTENT_TYPE
+    msg_added = _(u"Video added")
+    label = _(u"Add video")
+
+    @button.buttonAndHandler(_(u"Add video"))
+    def handleAdd(self, action):
+        group_base.BaseAddForm.handleAdd(self, action)
+
+
+class VideosView(group_base.BaseAddFormView):
+    """A filterable blog view"""
+    filter_type = [CONTENT_TYPE]
+    form = AddForm
 
 
 class NavigationRootVideosView(VideosView, NavigationRootBaseView):
