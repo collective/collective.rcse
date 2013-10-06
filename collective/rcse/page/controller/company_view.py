ef664111e1ef90484b8e4672d6f4494fa217b7b0
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-10-06 19:00:15 +0200

    feature(comapny): use timeline_view for company
    display a timeline with all members of the timeline

--- collective/rcse/page/controller/company_view.py
+++ collective/rcse/page/controller/company_view.py
@@ -1,5 +1,6 @@
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
+from collective.rcse.page.controller.timeline_view import TimelineView
 
 
 class CompanyInfoView(BrowserView):
@@ -12,7 +13,7 @@ class CompanyInfoView(BrowserView):
 
     def __call__(self):
         self.update()
-        self.getCompanyProperties()
+        self.url = self.context.absolute_url()
         if self.__name__.endswith("_view"):
             return self.index()
         return self
@@ -25,9 +26,32 @@ class CompanyInfoView(BrowserView):
         if self.catalog is None:
             self.catalog = getToolByName(self.context, 'portal_catalog')
 
-    def getCompanyProperties(self):
-        self.url = '/'.join(self.context.getPhysicalPath())
 
-    def getUsers(self):
-        brains = self.catalog(company_id=self.context.id)
-        return [brain.getObject() for brain in brains]
+class CompanyTimelineView(BrowserView):
+    is_content_timeline = True
+
+    def __call__(self):
+        self.update()
+        return self.index()
+
+    def update(self):
+        self.catalog = getToolByName(self.context, 'portal_catalog')
+        self.portal_url = getToolByName(self.context, 'portal_url')
+
+        self.group = self.context
+        self.group_title = self.context.Title()
+        name = "@@company_description_view"
+        self.group_description = self.context.restrictedTraverse(name)()
+        self.group_url = self.group.absolute_url()
+        if self.context.logo:
+            self.group_photo = "%s/@@images/logo" % self.group_url
+        else:
+            self.group_photo = None
+        self.group_actions = None
+        name = "@@collective.rcse.editbar"
+        self.group_edit_bar = self.context.restrictedTraverse(name)
+        name = "@@plone.abovecontenttitle.documentactions"
+        self.group_actions = self.group.restrictedTraverse(name)
+
+    def get_content(self, **kwargs):
+        return self.catalog(company_id=self.context.getId())
