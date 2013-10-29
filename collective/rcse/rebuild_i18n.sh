aa6cb6059b26d9f6db29a2fde9f72de85c4c3d7c
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-10-29 12:03:46 +0100

    fix(i18n): add all wf transitions in plone domains

--- collective/rcse/rebuild_i18n.sh
+++ collective/rcse/rebuild_i18n.sh
@@ -7,3 +7,5 @@ i18ndude rebuild-pot --exclude resources --pot locales/${PRODUCTNAME}.pot --crea
 
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
+
+i18ndude sync --pot locales/plone.pot locales/*/LC_MESSAGES/plone.po
