f850819a59d0f24e6055df56ea8f39c2fb3d7e44
Gagaro <gagaro42@gmail.com>
2013-11-07 16:33:51 +0100

    fix: subscribe creator to his content #10405

--- collective/rcse/workflow.py
+++ collective/rcse/workflow.py
@@ -2,8 +2,20 @@ from Products.CMFPlone.utils import getToolByName
 from zope.component import getMultiAdapter
 from zope.globalrequest import getRequest
 
+from collective.whathappened.storage_manager import StorageManager
+from collective.whathappened.subscription import Subscription
 from collective.rcse.i18n import _
 from collective.rcse.content.utils import createCompany
+
+
+def handle_content_creation(context, event):
+    context_path = '/'.join(context.getPhysicalPath())
+    user = context.Creator
+    storage = StorageManager(context)
+    storage.initialize()
+    storage.setUser(user)
+    storage.saveSubscription(Subscription(context_path, True))
+    storage.terminate()
 
 
 def handle_user_validation(context, event):
