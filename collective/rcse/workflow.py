4834a5155e4a2bfa5401d238c8624566ee98e258
Gagaro <gagaro42@gmail.com>
2013-11-07 17:36:32 +0100

    fix: subscribe when created only if context has a creator

--- collective/rcse/workflow.py
+++ collective/rcse/workflow.py
@@ -1,3 +1,5 @@
+import logging
+
 from Products.CMFPlone.utils import getToolByName
 from zope.component import getMultiAdapter
 from zope.globalrequest import getRequest
@@ -7,10 +9,15 @@ from collective.whathappened.subscription import Subscription
 from collective.rcse.i18n import _
 from collective.rcse.content.utils import createCompany
 
+logger = logging.getLogger(__name__)
+
 
 def handle_content_creation(context, event):
     context_path = '/'.join(context.getPhysicalPath())
-    user = context.Creator
+    user = context.Creator()
+    if not user:
+        logger.warning('context %s has no creator.' % context)
+        return
     storage = StorageManager(context)
     storage.initialize()
     storage.setUser(user)
