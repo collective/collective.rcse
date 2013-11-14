9695b9308b3221177ab88327568acaf8272e3b05
Gagaro <gagaro42@gmail.com>
2013-11-14 15:17:16 +0100

    fix: tooltip in portlet calendar #10470

--- collective/rcse/resources/dev/js/theme-desktop.js
+++ collective/rcse/resources/dev/js/theme-desktop.js
@@ -43,6 +43,7 @@ var rcseInitFluidMedia = function(){
         rcseUpdateFluidMedia($(event.target).parent());
     });
 }
+
 var rcseUpdatePortlets = function(element) {
     if (element == undefined) {
         element = document;
@@ -95,9 +96,16 @@ var rcseUpdatePortlets = function(element) {
             );
             //do not add list-group-item
             $(newList).append(portlet.find(".portletItem").html());
-            portlet.find('.event a').tooltip({html:true, delay: {show: 0, hide: 2000}});
             newPortlet.appendChild(newList);
-
+            $(newPortlet).find('.event a').tooltip(
+		{
+		    html:true,
+		    delay: {
+			show: 0,
+			hide: 2000
+		    }
+		}
+	    );
         }else if (portlet.hasClass('portletNavigationTree')) {
             portlet.find('a').addClass('list-group-item');
             portlet.find('div > a').unwrap();
