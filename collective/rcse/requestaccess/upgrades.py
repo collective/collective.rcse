e0217d2c91aaddde46ee56aef7ab1de129e096a5
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-11-22 19:58:43 +0100

    fix: remove other stuff from the merge with requestaccess

--- collective/rcse/requestaccess/upgrades.py
+++
@@ -1,7 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-PROFILE = 'profile-collective.requestaccess:default'
-
-
-def common(context):
-    setup = getToolByName(context, 'portal_setup')
-    setup.runAllImportStepsFromProfile(PROFILE)
