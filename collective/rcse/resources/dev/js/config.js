bde6c192982ce2cf48745836affc76303b0edde0
Gagaro <gagaro42@gmail.com>
2014-03-10 16:13:55 +0100

    fix: ckeditor config

--- collective/rcse/resources/dev/js/config.js
+++ collective/rcse/resources/dev/js/config.js
@@ -1,5 +1,8 @@
 
 CKEDITOR.editorConfig = function( config ) {
+    config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,clipboard,button,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,div,resize,toolbar,elementspath,enterkey,entities,popup,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc,oembed,mediaembed,sourcedialog,texttransform';
+    config.skin = 'moono';
+
     config.language = 'fr';
     config.removePlugins = 'sourcearea,oembed';
 };
